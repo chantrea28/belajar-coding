a6d6cfd242a6d8f9b976d20720aaf380f6ed7455
chantrea28 <chantreamahardika@gmail.com>
2026-01-22 19:37:29 +0700

    Add files via upload

---
+++ python-data-types.py
@@ -0,0 +1,14 @@
+nama = 'Yadi'
+print(type(nama))
+
+umur= 30
+print(type(umur))
+
+bb = 54.7
+print(type(bb))
+
+active = True
+print(type(active))
+
+animals = ['chicken','fish','cat','dog']
+print(type(animals))
