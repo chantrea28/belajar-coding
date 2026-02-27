512410586eeac998d74fdbef9542561cf02f3d9f
chantrea28 <chantreamahardika@gmail.com>
2026-02-27 13:39:23 +0700

    Add files via upload

--- latihan 8.py
+++ latihan 8.py
@@ -16,3 +16,41 @@ print(fruits[2])
 print(name[2])
 
 print(cars[0])
+
+fruits[2] = 'durian'
+print(fruits)
+
+fruits.append('melon')
+print(fruits)
+
+fruits.remove('banana')
+print(fruits)
+
+fruits.clear()
+print(fruits)
+
+country = list(("indonesia","malaysia","jepang"))
+print(country)
+print(type(country))
+
+print(20*'=')
+print('tupple')
+
+animals = ("fish","lion","tutle")
+print(animals)
+print(type(animals))
+
+koordinat = (-90,123,103.678)
+
+print(animals[0])
+print(animals[2])
+
+#koordinat[0] = 80.567
+
+koordinat_list =  list(koordinat)
+print(koordinat_list)
+koordinat_list[0] = 80.567
+print(koordinat_list)
+
+koordinat = tuple(koordinat_list)
+print(koordinat)
---
+++ projectlist.py
@@ -0,0 +1,9 @@
+foods = input('favorite food1:')
+foods = input('favorite food2:')
+foods = input('favorite food3:')
+
+foods = []
+foods.append('favorite food1')
+foods.append('favorite food2')
+foods.append('favorite food3')
+print(foods)
