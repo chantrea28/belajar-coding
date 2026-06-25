28f5b9718f73a47cf2f0cbb66abd210ec0c78f2a
chantrea28 <chantreamahardika@gmail.com>
2026-06-25 19:59:35 +0700

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,10 @@
+lambda name : print(f"hello,{name}")
+
+
+hello = lambda name: print(f"hello,{name}")
+
+hello ("ilham")
+
+
+is_genap = lambda number :print("yes") if number % 2 == 0 else print("no")
+is_genap (5)
---
+++ latihan 19.py
@@ -0,0 +1,16 @@
+add = lambda number1, number2 : print(f" hasilnya adalah ...{number1 + number2} = {add}")
+
+add(7,9)
+
+
+subs = lambda number1, number2 : print(f"hasilnya adalah...{number1 - number2}")
+
+subs(9,7)
+
+multi = lambda number1, number2 : print(f"hasilnya adalah...{number1 * number2}")
+
+multi(9,7)
+
+div = lambda number1, number2 : print(f"hasilnya adalah...{number1 / number2}")
+
+div(10,2)
